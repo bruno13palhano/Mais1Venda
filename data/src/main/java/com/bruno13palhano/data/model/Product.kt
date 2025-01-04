b852acc094c1384e181a811b438c3161fc6618e2
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 11:44:46 -0300

    Add Product

---
+++ data/src/main/java/com/bruno13palhano/data/model/Product.kt
@@ -0,0 +1,12 @@
+package com.bruno13palhano.data.model
+
+data class Product(
+    val name: String,
+    val price: Float,
+    val category: List<String>,
+    val description: String,
+    val code: String,
+    val quantity: Int,
+    val exhibitToCatalog: Boolean,
+    val timestamp: Long
+)
