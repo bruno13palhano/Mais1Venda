8b08711834009fec37976471a59bae8159a189ae
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 11:44:07 -0300

    Add Customer

---
+++ data/src/main/java/com/bruno13palhano/data/model/Customer.kt
@@ -0,0 +1,9 @@
+package com.bruno13palhano.data.model
+
+data class Customer(
+    val uid: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val address: String
+)
