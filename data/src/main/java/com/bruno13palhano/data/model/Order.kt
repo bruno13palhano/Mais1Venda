0cdf1baea474a304a8349b2facf571fd4154d760
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 11:45:00 -0300

    Add Order

---
+++ data/src/main/java/com/bruno13palhano/data/model/Order.kt
@@ -0,0 +1,11 @@
+package com.bruno13palhano.data.model
+
+data class Order(
+    val id: Long,
+    val product: Product,
+    val customer: Customer,
+    val orderDate: Long,
+    val deliveryDate: Long,
+    val status: OrderStatus,
+    val timestamp: Long
+)
