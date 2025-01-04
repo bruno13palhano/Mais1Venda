2065591106225b1430b91f90dde36ea142516a73
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 11:44:27 -0300

    Add OrderStatus

---
+++ data/src/main/java/com/bruno13palhano/data/model/OrderStatus.kt
@@ -0,0 +1,16 @@
+package com.bruno13palhano.data.model
+
+enum class OrderStatus {
+    AWAITING_PAYMENT,
+    PROCESSING_PAYMENT,
+    PAYMENT_FAILED,
+    PAYMENT_CONFIRMED,
+    PROCESSING_ORDER,
+    AWAITING_DELIVERY,
+    AWAITING_CONFIRMATION,
+    AWAITING_PICKUP,
+    DELIVERED,
+    PICKED_UP,
+    CANCELED,
+    FINISHED
+}
