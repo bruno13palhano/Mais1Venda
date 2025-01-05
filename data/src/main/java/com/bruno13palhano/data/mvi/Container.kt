56589a25bba03e03cbfc1252bad7c07ea6f79779
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 21:32:45 -0300

    Add base MVI

---
+++ data/src/main/java/com/bruno13palhano/data/mvi/Container.kt
@@ -0,0 +1,43 @@
+package com.bruno13palhano.data.mvi
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class Container<STATE, EFFECT>(
+    initialState: STATE,
+    private val scope: CoroutineScope
+) {
+    private val _state: MutableStateFlow<STATE> = MutableStateFlow(initialState)
+    val state: StateFlow<STATE> = _state.asStateFlow()
+
+    private val _sideEffect = Channel<EFFECT>(Channel.CONFLATED)
+    val sideEffect: Flow<EFFECT> = _sideEffect.receiveAsFlow()
+
+    fun intent(transform: suspend Container<STATE, EFFECT>.() -> Unit) {
+        scope.launch(SINGLE_THREAD) {
+            this@Container.transform()
+        }
+    }
+
+    suspend fun reduce(reducer: suspend STATE.() -> STATE) {
+        withContext(SINGLE_THREAD) {
+            _state.value = _state.value.reducer()
+        }
+    }
+
+    suspend fun postSideEffect(effect: EFFECT) {
+        _sideEffect.send(effect)
+    }
+
+    companion object {
+        private val SINGLE_THREAD = Dispatchers.IO.limitedParallelism(1)
+    }
+}
