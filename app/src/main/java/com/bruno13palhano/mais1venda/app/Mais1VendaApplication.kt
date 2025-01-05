4248afab7f103e30ecbd1ecc9bae644bc02b12ce
bruno13palhano <bruno13palhano@gmail.com>
2025-01-04 21:30:43 -0300

    Config Hilt

---
+++ app/src/main/java/com/bruno13palhano/mais1venda/app/Mais1VendaApplication.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.mais1venda.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Mais1VendaApplication : Application()
