3498553adb1a020fff5cb924229b5841b1d41fd7
bruno13palhano <bruno13palhano@gmail.com>
2025-01-07 15:19:49 -0300

    Implement LoginScreen

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/login/presenter/LoginScreen.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/login/presenter/LoginScreen.kt
@@ -14,8 +14,9 @@ import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -23,30 +24,60 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.bruno13palhano.mais1venda.ui.screens.components.CircularProgress
+import com.bruno13palhano.mais1venda.R
 import com.bruno13palhano.mais1venda.ui.screens.authentication.login.viewmodel.LoginViewModel
+import com.bruno13palhano.mais1venda.ui.screens.components.CircularProgress
+import com.bruno13palhano.mais1venda.ui.screens.components.CustomPasswordTextField
+import com.bruno13palhano.mais1venda.ui.screens.components.CustomTextField
+import com.bruno13palhano.mais1venda.ui.screens.shared.clickableWithoutRipple
 import com.bruno13palhano.mais1venda.ui.screens.shared.rememberFlowWithLifecycle
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun LoginRoute(
     navigateToHome: () -> Unit,
+    navigateToForgotPassword: () -> Unit,
+    navigateToCreateAccount: () -> Unit,
     viewModel: LoginViewModel = hiltViewModel(),
 ) {
     val state = viewModel.container.state.collectAsStateWithLifecycle()
     val effect = rememberFlowWithLifecycle(viewModel.container.sideEffect)
+
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
 
     LaunchedEffect(effect) {
         effect.collect { sideEffect ->
             when (sideEffect) {
                 LoginSideEffect.NavigateToHome -> navigateToHome()
 
+                LoginSideEffect.NavigateToForgotPassword -> navigateToForgotPassword()
+
+                LoginSideEffect.NavigateToCreateAccount -> navigateToCreateAccount()
+
+                LoginSideEffect.DismissKeyboard -> {
+                    focusManager.clearFocus()
+                    keyboardController?.hide()
+                }
+
                 is LoginSideEffect.ShowError -> {
-                    println("Error: ${sideEffect.message}")
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = sideEffect.message,
+                            withDismissAction = true,
+                        )
+                    }
                 }
             }
         }
@@ -54,6 +85,7 @@ internal fun LoginRoute(
 
     LoginContent(
         state = state.value,
+        snackbarHostState = snackbarHostState,
         onEvent = viewModel::handleEvent,
     )
 }
@@ -62,15 +94,20 @@ internal fun LoginRoute(
 @Composable
 private fun LoginContent(
     state: LoginState,
+    snackbarHostState: SnackbarHostState,
     onEvent: (LoginEvent) -> Unit,
 ) {
     Scaffold(
-        modifier = Modifier.consumeWindowInsets(WindowInsets.safeDrawing),
+        modifier =
+            Modifier
+                .clickableWithoutRipple { onEvent(LoginEvent.DismissKeyboard) }
+                .consumeWindowInsets(WindowInsets.safeDrawing),
         topBar = {
             TopAppBar(
-                title = { Text(text = "Login") },
+                title = { Text(text = stringResource(R.string.login)) },
             )
         },
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         floatingActionButton = {
             FloatingActionButton(onClick = { onEvent(LoginEvent.Login) }) {
                 Icon(imageVector = Icons.Filled.Done, contentDescription = "Done")
@@ -96,30 +133,32 @@ private fun LoginContent(
                         .consumeWindowInsets(it)
                         .verticalScroll(rememberScrollState()),
             ) {
-                OutlinedTextField(
-                    modifier =
-                        Modifier
-                            .padding(horizontal = 8.dp)
-                            .fillMaxWidth(),
+                CustomTextField(
+                    Modifier
+                        .padding(horizontal = 8.dp)
+                        .fillMaxWidth(),
                     value = email,
                     onValueChange = { newEmail ->
                         email = newEmail
                         onEvent(LoginEvent.EmailChanged(email = newEmail))
                     },
-                    label = { Text(text = "Email") },
+                    label = stringResource(R.string.email),
+                    placeholder = stringResource(R.string.enter_email),
                 )
 
-                OutlinedTextField(
-                    modifier =
-                        Modifier
-                            .padding(horizontal = 8.dp)
-                            .fillMaxWidth(),
+                CustomPasswordTextField(
+                    Modifier
+                        .padding(horizontal = 8.dp)
+                        .fillMaxWidth(),
+                    visibility = state.passwordVisibility,
                     value = password,
                     onValueChange = { newPassword ->
                         password = newPassword
                         onEvent(LoginEvent.PasswordChanged(password = newPassword))
                     },
-                    label = { Text(text = "Password") },
+                    togglePasswordVisibility = { onEvent(LoginEvent.TogglePasswordVisibility) },
+                    label = stringResource(R.string.password),
+                    placeholder = stringResource(R.string.enter_password),
                 )
             }
         }
