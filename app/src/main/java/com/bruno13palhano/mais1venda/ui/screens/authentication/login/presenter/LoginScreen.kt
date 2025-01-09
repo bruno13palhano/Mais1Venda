125649baeee091e0ad879acd589b9ae13dac119b
bruno13palhano <bruno13palhano@gmail.com>
2025-01-09 20:23:53 -0300

    Refactor login button and create buttons for CreateAccount and ForgotPassword events

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/login/presenter/LoginScreen.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/login/presenter/LoginScreen.kt
@@ -1,6 +1,8 @@
 package com.bruno13palhano.mais1venda.ui.screens.authentication.login.presenter
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,24 +11,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -105,11 +112,6 @@ private fun LoginContent(
             )
         },
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { onEvent(LoginEvent.Login) }) {
-                Icon(imageVector = Icons.Filled.Done, contentDescription = "Done")
-            }
-        },
     ) {
         if (state.isLoading) {
             CircularProgress(
@@ -124,6 +126,7 @@ private fun LoginContent(
                     Modifier
                         .padding(it)
                         .consumeWindowInsets(it)
+                        .fillMaxSize()
                         .verticalScroll(rememberScrollState()),
             ) {
                 CustomTextField(
@@ -149,7 +152,79 @@ private fun LoginContent(
                     placeholder = stringResource(R.string.enter_password),
                     isError = state.passwordError,
                 )
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    TextButton(onClick = { onEvent(LoginEvent.ForgotPassword) }) {
+                        Text(
+                            text = stringResource(id = R.string.forgot_your_password),
+                            textAlign = TextAlign.End,
+                            fontWeight = FontWeight.Medium,
+                        )
+                    }
+                }
+
+                Button(
+                    modifier =
+                        Modifier
+                            .padding(start = 24.dp, top = 32.dp, bottom = 8.dp, end = 24.dp)
+                            .fillMaxWidth(),
+                    onClick = { onEvent(LoginEvent.Login) },
+                ) {
+                    Text(text = stringResource(id = R.string.login))
+                }
+
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    HorizontalDivider(
+                        modifier =
+                            Modifier
+                                .padding(12.dp)
+                                .weight(1f),
+                    )
+                    Text(
+                        text = stringResource(R.string.or),
+                        fontWeight = FontWeight.Medium,
+                    )
+                    HorizontalDivider(
+                        modifier =
+                            Modifier
+                                .padding(12.dp)
+                                .weight(1f),
+                    )
+                }
+
+                Button(
+                    modifier =
+                        Modifier
+                            .padding(start = 24.dp, top = 8.dp, bottom = 32.dp, end = 24.dp)
+                            .fillMaxWidth(),
+                    onClick = { onEvent(LoginEvent.CreateAccount) },
+                ) {
+                    Text(text = stringResource(id = R.string.create_account))
+                }
             }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun LoginContentPreview() {
+    MaterialTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            LoginContent(
+                state = LoginState(),
+                snackbarHostState = remember { SnackbarHostState() },
+                onEvent = {},
+            )
         }
     }
 }
