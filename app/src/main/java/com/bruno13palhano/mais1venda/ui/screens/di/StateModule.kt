3980887704d1eacf8b1404e2065bf0214eefc5fe
bruno13palhano <bruno13palhano@gmail.com>
2025-01-15 10:40:37 -0300

    Add HomeState and SettingsState to StateModule for DI

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/di/StateModule.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/di/StateModule.kt
@@ -2,6 +2,8 @@ package com.bruno13palhano.mais1venda.ui.screens.di
 
 import com.bruno13palhano.mais1venda.ui.screens.authentication.create.presenter.CreateAccountState
 import com.bruno13palhano.mais1venda.ui.screens.authentication.login.presenter.LoginState
+import com.bruno13palhano.mais1venda.ui.screens.home.presenter.HomeState
+import com.bruno13palhano.mais1venda.ui.screens.settings.presenter.SettingsState
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,9 +15,17 @@ import javax.inject.Singleton
 internal object StateModule {
     @Provides
     @Singleton
-    fun bindLoginState() = LoginState()
+    fun provideHomeState() = HomeState()
 
     @Provides
     @Singleton
-    fun bindCreateAccountState() = CreateAccountState()
+    fun provideLoginState() = LoginState()
+
+    @Provides
+    @Singleton
+    fun provideCreateAccountState() = CreateAccountState()
+
+    @Provides
+    @Singleton
+    fun provideSettingsState() = SettingsState()
 }
