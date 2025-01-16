babfad36bf8859b9ad6a35bf30f4a414964ce47d
bruno13palhano <bruno13palhano@gmail.com>
2025-01-15 21:27:43 -0300

    Make CompanyRepository and ProductRepository injectable by hilt

--- data/src/main/java/com/bruno13palhano/data/di/RepositoryModule.kt
+++ data/src/main/java/com/bruno13palhano/data/di/RepositoryModule.kt
@@ -2,6 +2,10 @@ package com.bruno13palhano.data.di
 
 import com.bruno13palhano.data.repository.CompanyRepository
 import com.bruno13palhano.data.repository.CompanyRepositoryImpl
+import com.bruno13palhano.data.repository.OrderRepository
+import com.bruno13palhano.data.repository.OrderRepositoryImpl
+import com.bruno13palhano.data.repository.ProductRepository
+import com.bruno13palhano.data.repository.ProductRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -9,6 +13,10 @@ import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 annotation class CompanyRep
+
+annotation class OrderRep
+
+annotation class ProductRep
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -19,4 +27,18 @@ internal abstract class RepositoryModule {
     abstract fun bindCompanyRepository(
         companyRepositoryImpl: CompanyRepositoryImpl,
     ): CompanyRepository
+
+    @Binds
+    @OrderRep
+    @Singleton
+    abstract fun bindOrderRepository(
+        orderRepositoryImpl: OrderRepositoryImpl,
+    ): OrderRepository
+
+    @Binds
+    @ProductRep
+    @Singleton
+    abstract fun bindProductRepository(
+        productRepositoryImpl: ProductRepositoryImpl,
+    ): ProductRepository
 }
