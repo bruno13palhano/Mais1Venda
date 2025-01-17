9acc7396628fe18c4fa25ccebbfc6e9ad786285e
bruno13palhano <bruno13palhano@gmail.com>
2025-01-16 21:20:42 -0300

    Refactor

--- data/src/main/java/com/bruno13palhano/data/datasource/local/dao/ProductDao.kt
+++ data/src/main/java/com/bruno13palhano/data/datasource/local/dao/ProductDao.kt
@@ -6,23 +6,23 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.bruno13palhano.data.datasource.local.data.ProductLocalData
-import com.bruno13palhano.data.model.company.Product
+import com.bruno13palhano.data.datasource.local.entity.ProductEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-internal interface ProductDao : ProductLocalData {
+internal interface ProductDao : ProductLocalData<ProductEntity> {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    override suspend fun insert(product: Product)
+    override suspend fun insert(product: ProductEntity)
 
     @Update
-    override suspend fun update(product: Product)
+    override suspend fun update(product: ProductEntity)
 
     @Query("DELETE FROM product WHERE id = :id")
     override suspend fun delete(id: Long)
 
     @Query("SELECT * FROM product WHERE id = :id")
-    override suspend fun getById(id: Long): Product?
+    override suspend fun getById(id: Long): ProductEntity?
 
     @Query("SELECT * FROM product")
-    override fun getAll(): Flow<List<Product>>
+    override fun getAll(): Flow<List<ProductEntity>>
 }
--- data/src/main/java/com/bruno13palhano/data/datasource/local/data/ProductLocalData.kt
+++ data/src/main/java/com/bruno13palhano/data/datasource/local/data/ProductLocalData.kt
@@ -1,16 +1,15 @@
 package com.bruno13palhano.data.datasource.local.data
 
-import com.bruno13palhano.data.model.company.Product
 import kotlinx.coroutines.flow.Flow
 
-internal interface ProductLocalData {
-    suspend fun insert(product: Product)
+internal interface ProductLocalData<T> {
+    suspend fun insert(product: T)
 
-    suspend fun update(product: Product)
+    suspend fun update(product: T)
 
     suspend fun delete(id: Long)
 
-    suspend fun getById(id: Long): Product?
+    suspend fun getById(id: Long): T?
 
-    fun getAll(): Flow<List<Product>>
+    fun getAll(): Flow<List<T>>
 }
