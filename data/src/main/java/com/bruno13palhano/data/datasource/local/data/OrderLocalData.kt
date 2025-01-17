0819839cecd35205dcdd870705e38a8afb1ee95d
bruno13palhano <bruno13palhano@gmail.com>
2025-01-16 21:21:34 -0300

    Refactor

--- data/src/main/java/com/bruno13palhano/data/datasource/local/data/OrderLocalData.kt
+++ data/src/main/java/com/bruno13palhano/data/datasource/local/data/OrderLocalData.kt
@@ -1,16 +1,15 @@
 package com.bruno13palhano.data.datasource.local.data
 
-import com.bruno13palhano.data.model.shared.Order
 import kotlinx.coroutines.flow.Flow
 
-internal interface OderLocalData {
-    suspend fun insert(order: Order)
+internal interface OrderLocalData<T> {
+    suspend fun insert(order: T)
 
-    suspend fun update(order: Order)
+    suspend fun update(order: T)
 
     suspend fun delete(id: Long)
 
-    suspend fun getById(id: Long): Order?
+    suspend fun getById(id: Long): T?
 
-    fun getAll(): Flow<List<Order>>
+    fun getAll(): Flow<List<T>>
 }
