0c241e3e574256b50176dfb9478903215748e5cf
bruno13palhano <bruno13palhano@gmail.com>
2025-01-17 22:46:59 -0300

    Add hilt ServiceModule

---
+++ data/src/main/java/com/bruno13palhano/data/di/ServiceModule.kt
@@ -0,0 +1,47 @@
+package com.bruno13palhano.data.di
+
+import com.bruno13palhano.data.BuildConfig
+import com.bruno13palhano.data.datasource.remote.service.Service
+import com.bruno13palhano.data.datasource.remote.service.ServiceImpl
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+private const val SERVER_URL = BuildConfig.serverUrl
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideService(): Service {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(SERVER_URL)
+            .client(client)
+            .build()
+
+        val apiService: Service by lazy { retrofit.create(ServiceImpl::class.java) }
+
+        return apiService
+    }
+}
