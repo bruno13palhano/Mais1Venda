2bd02bfd65296d48732babe26993fa9814dbf6d2
bruno13palhano <bruno13palhano@gmail.com>
2025-01-19 13:25:20 -0300

    Fix missing context injetion

--- data/src/main/java/com/bruno13palhano/data/di/DatabaseModule.kt
+++ data/src/main/java/com/bruno13palhano/data/di/DatabaseModule.kt
@@ -6,16 +6,20 @@ import com.bruno13palhano.data.datasource.local.database.AppDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object DatabaseModule {
     @Provides
+    @Singleton
     fun provideCompanyDao(appDatabase: AppDatabase) = appDatabase.companyDao()
 
     @Provides
-    fun provideAppDatabase(context: Context): AppDatabase {
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
         return Room.databaseBuilder(
             context,
             AppDatabase::class.java,
