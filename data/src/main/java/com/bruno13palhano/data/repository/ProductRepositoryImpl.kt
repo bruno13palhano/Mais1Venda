f441706592d4c21b48bbd2d4ea937aecbd41f5bd
bruno13palhano <bruno13palhano@gmail.com>
2025-01-29 21:54:21 -0300

    Implement ProductRepository

--- data/src/main/java/com/bruno13palhano/data/repository/ProductRepositoryImpl.kt
+++ data/src/main/java/com/bruno13palhano/data/repository/ProductRepositoryImpl.kt
@@ -1,23 +1,63 @@
 package com.bruno13palhano.data.repository
 
+import com.bruno13palhano.data.datasource.local.dao.ProductDao
+import com.bruno13palhano.data.datasource.remote.source.ProductRemoteData
 import com.bruno13palhano.data.model.company.Product
+import com.bruno13palhano.data.model.company.asExternal
+import com.bruno13palhano.data.model.company.asInternal
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-internal class ProductRepositoryImpl @Inject constructor() : ProductRepository {
+internal class ProductRepositoryImpl @Inject constructor(
+    private val productRemoteData: ProductRemoteData,
+    private val productDao: ProductDao,
+) : ProductRepository {
     override suspend fun insert(product: Product) {
-        TODO("Not yet implemented")
+        productDao.insert(product = product.asInternal()).let {
+            val response = productRemoteData.insert(product.copy(id = it))
+            response.data?.let { success ->
+                if (success) {
+                    // TODO: log info?
+                } else {
+                    // TODO: retry and log info?
+                }
+            }
+        }
     }
 
     override suspend fun update(product: Product) {
-        TODO("Not yet implemented")
+        productDao.update(product = product.asInternal()).let {
+            // whether the table was updated successfully
+            if (it > 0) {
+                val response = productRemoteData.update(product = product)
+                response.data?.let { success ->
+                    if (success) {
+                        // TODO: log info?
+                    } else {
+                        // TODO: retry and log info?
+                    }
+                }
+            } else {
+                // TODO: retry and log info?
+            }
+        }
     }
 
     override suspend fun delete(id: Long) {
-        TODO("Not yet implemented")
+        productDao.delete(id = id).let {
+            val response = productRemoteData.deleteById(id = id)
+            response.data?.let { success ->
+                if (success) {
+                    // TODO: log info?
+                } else {
+                    // TODO: retry and log info?
+                }
+            }
+        }
     }
 
     override suspend fun getAll(): Flow<List<Product>> {
-        TODO("Not yet implemented")
+        return productDao.getAll().map { it.map { product -> product.asExternal() } }
     }
 }
