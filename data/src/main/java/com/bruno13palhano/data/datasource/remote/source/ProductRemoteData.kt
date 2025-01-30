e42f76d72dfdb116a86b2461cf634b08a8798424
bruno13palhano <bruno13palhano@gmail.com>
2025-01-29 21:55:02 -0300

    Implement ProductRemoteData

--- data/src/main/java/com/bruno13palhano/data/datasource/remote/source/ProductRemoteData.kt
+++ data/src/main/java/com/bruno13palhano/data/datasource/remote/source/ProductRemoteData.kt
@@ -1,25 +1,26 @@
 package com.bruno13palhano.data.datasource.remote.source
 
+import com.bruno13palhano.data.datasource.remote.service.ApiService
 import com.bruno13palhano.data.model.company.Product
+import com.bruno13palhano.data.model.resource.Resource
+import javax.inject.Inject
 
-class ProductRemoteData {
-    suspend fun insert(product: Product): Product {
-        TODO("Not yet implemented")
+internal class ProductRemoteData @Inject constructor(
+    private val api: ApiService,
+) {
+    suspend fun insert(product: Product): Resource<Boolean> {
+        return safeApiCall { api.insertProduct(product = product) }
     }
 
-    suspend fun update(product: Product): Product {
-        TODO("Not yet implemented")
+    suspend fun update(product: Product): Resource<Boolean> {
+        return safeApiCall { api.updateProduct(product = product) }
     }
 
-    suspend fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+    suspend fun deleteById(id: Long): Resource<Boolean> {
+        return safeApiCall { api.deleteProductById(id = id) }
     }
 
-    suspend fun getById(id: Long): Product? {
-        TODO("Not yet implemented")
-    }
-
-    suspend fun getAll(): List<Product> {
-        TODO("Not yet implemented")
+    suspend fun getAll(): Resource<List<Product>> {
+        return safeApiCall { api.getAllProducts() }
     }
 }
