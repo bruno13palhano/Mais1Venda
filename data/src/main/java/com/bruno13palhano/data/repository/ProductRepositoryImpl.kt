68324bd5f9ae57ac5dc5351d9d3c92696d1869e7
bruno13palhano <bruno13palhano@gmail.com>
2025-01-31 20:40:25 -0300

    Refactor ProductRepositoryImpl

--- data/src/main/java/com/bruno13palhano/data/repository/ProductRepositoryImpl.kt
+++ data/src/main/java/com/bruno13palhano/data/repository/ProductRepositoryImpl.kt
@@ -10,39 +10,30 @@ import com.bruno13palhano.data.model.resource.ErrorType
 import com.bruno13palhano.data.model.resource.Resource
 import javax.inject.Inject
 
-private const val SERVER_TAG = "ProductRepository-Remote"
+private const val REMOTE_TAG = "ProductRepository-Remote"
 private const val LOCAL_TAG = "ProductRepository-Local"
 
 internal class ProductRepositoryImpl @Inject constructor(
     private val productRemoteData: ProductRemoteData,
     private val productDao: ProductDao,
-    private val log: AppLog
+    private val log: AppLog,
 ) : ProductRepository {
     override suspend fun insert(product: Product): Resource<Boolean> {
         return try {
             val id = productDao.insert(product = product.asInternal())
             val response = productRemoteData.insert(product.copy(id = id))
 
-            if (id > 0) {
-                response.data?.let { success ->
-                    if (success) {
-                        log.logInfo(tag = SERVER_TAG, message = "Product inserted successfully")
-                    } else {
-                        log.logInfo(tag = SERVER_TAG, message = "Product not inserted")
-                    }
-                }
-
-                log.logInfo(tag = LOCAL_TAG, message = "Product inserted successfully")
-
-                Resource.Success(data = true)
-            } else {
+            if (id <= 0) {
                 log.logInfo(tag = LOCAL_TAG, message = "Product not inserted")
-
-                Resource.Success(data = false)
+                return Resource.Success(false)
             }
+            log.logInfo(tag = LOCAL_TAG, message = "Product inserted successfully")
+
+            response.data?.let { success -> logRemoteProductInsertion(success = success) }
+
+            Resource.Success(data = true)
         } catch (e: Exception) {
             log.logError(tag = LOCAL_TAG, message = e.message ?: "Unknown error")
-
             Resource.Error(errorType = ErrorType.UNKNOWN)
         }
     }
@@ -51,27 +42,18 @@ internal class ProductRepositoryImpl @Inject constructor(
         return try {
             val rowsAffected = productDao.update(product = product.asInternal())
 
-            if (rowsAffected > 0) {
-                val response = productRemoteData.update(product)
-                response.data?.let { success ->
-                    if (success) {
-                        log.logInfo(tag = SERVER_TAG, message = "Product updated successfully")
-                    } else {
-                        log.logInfo(tag = SERVER_TAG, message = "Product not updated")
-                    }
-                }
-
-                log.logInfo(tag = LOCAL_TAG, message = "Product updated successfully")
-
-                Resource.Success(data = true)
-            } else {
+            if (rowsAffected <= 0) {
                 log.logInfo(tag = LOCAL_TAG, message = "Product not updated")
-
-                Resource.Success(data = false)
+                return Resource.Success(false)
             }
+            log.logInfo(tag = LOCAL_TAG, message = "Product updated successfully")
+
+            val response = productRemoteData.update(product)
+            response.data?.let { success -> logRemoteProductUpdate(success = success) }
+
+            Resource.Success(data = true)
         } catch (e: Exception) {
             log.logError(tag = LOCAL_TAG, message = e.message ?: "Unknown error")
-
             Resource.Error(errorType = ErrorType.UNKNOWN)
         }
     }
@@ -80,24 +62,16 @@ internal class ProductRepositoryImpl @Inject constructor(
         return try {
             val rowsAffected = productDao.delete(id = id)
 
-            if (rowsAffected > 0) {
-                val response = productRemoteData.deleteById(id = id)
-                response.data?.let { success ->
-                    if (success) {
-                        log.logInfo(tag = SERVER_TAG, message = "Product deleted successfully")
-                    } else {
-                        log.logInfo(tag = SERVER_TAG, message = "Product not deleted")
-                    }
-                }
-
-                log.logInfo(tag = LOCAL_TAG, message = "Product deleted successfully")
-
-                return Resource.Success(true)
-            } else {
+            if (rowsAffected <= 0) {
                 log.logInfo(tag = LOCAL_TAG, message = "Product not deleted")
-
                 return Resource.Success(false)
             }
+            log.logInfo(tag = LOCAL_TAG, message = "Product deleted successfully")
+
+            val response = productRemoteData.deleteById(id = id)
+            response.data?.let { success -> logRemoteProductDeletion(success = success) }
+
+            return Resource.Success(true)
         } catch (e: Exception) {
             Resource.Error(errorType = ErrorType.UNKNOWN)
         }
@@ -106,14 +80,42 @@ internal class ProductRepositoryImpl @Inject constructor(
     override suspend fun getAll(): Resource<List<Product>> {
         return try {
             val products = productDao.getAll().map { it.asExternal() }
-
             log.logInfo(tag = LOCAL_TAG, message = "Products retrieved successfully")
 
             Resource.Success(data = products)
         } catch (e: Exception) {
             log.logError(tag = LOCAL_TAG, message = e.message ?: "Unknown error")
-
             Resource.Error(errorType = ErrorType.UNKNOWN)
         }
+    }
+
+    private fun logRemoteProductInsertion(success: Boolean) {
+        var message = "Product inserted successfully"
+
+        if (!success) {
+            message = "Product not inserted"
+        }
+
+        log.logInfo(tag = REMOTE_TAG, message = message)
+    }
+
+    private fun logRemoteProductUpdate(success: Boolean) {
+        var message = "Product updated successfully"
+
+        if (!success) {
+            message = "Product not updated"
+        }
+
+        log.logInfo(tag = REMOTE_TAG, message = message)
+    }
+
+    private fun logRemoteProductDeletion(success: Boolean) {
+        var message = "Product deleted successfully"
+
+        if (!success) {
+            message = "Product not deleted"
+        }
+
+        log.logInfo(tag = REMOTE_TAG, message = message)
     }
 }
