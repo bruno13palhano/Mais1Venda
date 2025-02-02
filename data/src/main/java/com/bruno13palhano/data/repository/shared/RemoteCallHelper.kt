e5a2ea0e67f91b43f6de18a99e36050f6d3660b4
bruno13palhano <bruno13palhano@gmail.com>
2025-02-02 17:06:41 -0300

    Fix retry calls

--- data/src/main/java/com/bruno13palhano/data/repository/shared/RemoteCallHelper.kt
+++ data/src/main/java/com/bruno13palhano/data/repository/shared/RemoteCallHelper.kt
@@ -6,20 +6,18 @@ import com.bruno13palhano.data.model.resource.Resource
  * @param retries: Number of retries
  * @param call: The initial api call to be called
  * @param success: Callback to be called when the api call is successful
- * @param retry: Callback to be retried if the api call fails
  */
 internal suspend fun <T> remoteCallWithRetry(
     retries: Int = 3,
     call: suspend () -> Resource<T>,
     success: (response: T?) -> Unit,
-    retry: suspend () -> Unit,
 ) {
-    val resource = call()
+    var resource = call()
     var remainingTries = retries
 
     while (resource.data == null && remainingTries > 0) {
         remainingTries--
-        retry()
+        resource = call()
     }
 
     if (resource.data != null) success(resource.data)
