b5ae4da7dd0afab7a0372464efd8734afa2b73f4
bruno13palhano <bruno13palhano@gmail.com>
2025-02-02 16:17:45 -0300

    Doc

--- data/src/main/java/com/bruno13palhano/data/repository/shared/RemoteCallHelper.kt
+++ data/src/main/java/com/bruno13palhano/data/repository/shared/RemoteCallHelper.kt
@@ -2,14 +2,20 @@ package com.bruno13palhano.data.repository.shared
 
 import com.bruno13palhano.data.model.resource.Resource
 
+/**
+ * @param retries: Number of retries
+ * @param call: The initial api call to be called
+ * @param success: Callback to be called when the api call is successful
+ * @param retry: Callback to be retried if the api call fails
+ */
 internal suspend fun <T> remoteCallWithRetry(
-    tries: Int = 3,
+    retries: Int = 3,
     call: suspend () -> Resource<T>,
     success: (response: T?) -> Unit,
     retry: suspend () -> Unit,
 ) {
     val resource = call()
-    var remainingTries = tries
+    var remainingTries = retries
 
     while (resource.data == null && remainingTries > 0) {
         remainingTries--
