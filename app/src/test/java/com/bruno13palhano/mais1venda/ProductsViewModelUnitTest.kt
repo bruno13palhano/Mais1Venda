b395c18c95e32a3384caae0864819a043a5b79a3
bruno13palhano <bruno13palhano@gmail.com>
2025-02-02 09:38:06 -0300

    Add TestProductRepository

--- app/src/test/java/com/bruno13palhano/mais1venda/ProductsViewModelUnitTest.kt
+++ app/src/test/java/com/bruno13palhano/mais1venda/ProductsViewModelUnitTest.kt
@@ -1,3 +1,22 @@
 package com.bruno13palhano.mais1venda
 
-internal class ProductsViewModelUnitTest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Before
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class ProductsViewModelUnitTest {
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(StandardTestDispatcher())
+    }
+
+    @Test
+    fun `test`() = runTest {
+    }
+}
