d4915efc28c70574c33bcaa9e9e56afde620baa2
bruno13palhano <bruno13palhano@gmail.com>
2025-02-04 16:27:50 -0300

    Add ProductsNavGraph

---
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/navigation/ProductsNavGraph.kt
@@ -0,0 +1,42 @@
+package com.bruno13palhano.mais1venda.ui.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.bruno13palhano.mais1venda.ui.screens.products.presenter.ProductsRoute
+import kotlinx.serialization.Serializable
+
+internal fun NavGraphBuilder.productsNavGraph(
+    navController: NavController,
+    gesturesEnabled: (enabled: Boolean) -> Unit,
+) {
+    gesturesEnabled(false)
+
+    navigation<HomeRoutes.Products>(startDestination = ProductsRoutes.MainProducts) {
+        composable<ProductsRoutes.MainProducts> {
+            ProductsRoute(
+                navigateToProduct = { navController.navigate(ProductsRoutes.Edit) },
+                navigateToNewProduct = { navController.navigate(ProductsRoutes.New) },
+                navigateBack = { navController.navigateUp() },
+            )
+        }
+
+        composable<ProductsRoutes.New> {
+        }
+
+        composable<ProductsRoutes.Edit> {
+        }
+    }
+}
+
+internal sealed interface ProductsRoutes {
+    @Serializable
+    data object MainProducts : ProductsRoutes
+
+    @Serializable
+    data object New : ProductsRoutes
+
+    @Serializable
+    data object Edit : ProductsRoutes
+}
