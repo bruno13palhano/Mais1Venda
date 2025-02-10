0c9b837b6c67f1ed0c0b96616d58ea30c6648edd
bruno13palhano <bruno13palhano@gmail.com>
2025-02-09 23:22:25 -0300

    Refactor CreateAccountViewModel

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/create/viewmodel/CreateAccountViewModel.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/authentication/create/viewmodel/CreateAccountViewModel.kt
@@ -60,73 +60,52 @@ internal class CreateAccountViewModel @Inject constructor(
     }
 
     private fun emailChanged(email: String) = container.intent {
-        reduce { copy(email = email) }
+        val emailError = !isEmailValid(email = email)
 
-        if (isEmailValid(email = email)) {
-            reduce { copy(emailError = false) }
-        } else {
-            reduce { copy(emailError = true) }
-        }
+        reduce { copy(email = email, emailError = emailError) }
     }
 
     private fun companyNameChanged(companyName: String) = container.intent {
-        reduce { copy(companyName = companyName) }
+        val companyNameError = !isCompanyNameValid(companyName = companyName)
 
-        if (isCompanyNameValid(companyName)) {
-            reduce { copy(companyNameError = false) }
-        } else {
-            reduce { copy(companyNameError = true) }
-        }
+        reduce { copy(companyName = companyName, companyNameError = companyNameError) }
     }
 
     private fun phoneChanged(phone: String) = container.intent {
-        reduce { copy(phone = phone) }
+        val phoneError = !isPhoneValid(phone = phone)
 
-        if (isPhoneValid(phone = phone)) {
-            reduce { copy(phoneError = false) }
-        } else {
-            reduce { copy(phoneError = true) }
-        }
+        reduce { copy(phone = phone, phoneError = phoneError) }
     }
 
     private fun addressChanged(address: String) = container.intent {
-        reduce { copy(address = address) }
+        val addressError = !isAddressValid(address = address)
 
-        if (address.isNotBlank()) {
-            reduce { copy(addressError = false) }
-        } else {
-            reduce { copy(addressError = true) }
-        }
+        reduce { copy(address = address, addressError = addressError) }
     }
 
     private fun passwordChanged(password: String) = container.intent {
-        reduce { copy(password = password) }
-        if (isPasswordValid(password = password)) {
-            reduce { copy(passwordError = false) }
+        val passwordError = !isPasswordValid(password = password)
+        val mismatchError = !isConfirmPasswordValid(
+            password = password,
+            confirmPassword = container.state.value.confirmPassword,
+        )
 
-            if (isConfirmPasswordValid(
-                    password = password,
-                    confirmPassword = container.state.value.confirmPassword,
-                )
-            ) {
-                reduce { copy(mismatchError = false) }
-            }
-        } else {
-            reduce { copy(passwordError = true) }
+        reduce {
+            copy(
+                password = password,
+                passwordError = passwordError,
+                mismatchError = mismatchError,
+            )
         }
     }
 
     private fun confirmPasswordChanged(confirmPassword: String) = container.intent {
-        reduce { copy(confirmPassword = confirmPassword) }
-        if (isConfirmPasswordValid(
-                password = container.state.value.password,
-                confirmPassword = confirmPassword,
-            )
-        ) {
-            reduce { copy(mismatchError = false) }
-        } else {
-            reduce { copy(mismatchError = true) }
-        }
+        val mismatchError = !isConfirmPasswordValid(
+            password = container.state.value.password,
+            confirmPassword = confirmPassword,
+        )
+
+        reduce { copy(confirmPassword = confirmPassword, mismatchError = mismatchError) }
     }
 
     private fun togglePasswordVisibility() = container.intent {
@@ -142,7 +121,7 @@ internal class CreateAccountViewModel @Inject constructor(
     }
 
     private fun createAccount() = container.intent {
-        if (!isFieldsValid()) {
+        if (isFieldsInvalid()) {
             handleInputErrorState()
             return@intent
         }
@@ -191,77 +170,86 @@ internal class CreateAccountViewModel @Inject constructor(
         postSideEffect(effect = CreateAccountSideEffect.NavigateBack)
     }
 
-    private fun isFieldsValid(): Boolean {
-        return isEmailValid(email = container.state.value.email) &&
-            isPasswordValid(password = container.state.value.password) &&
-            isConfirmPasswordValid(
-                password = container.state.value.password,
-                confirmPassword = container.state.value.confirmPassword,
-            ) &&
-            isCompanyNameValid(companyName = container.state.value.companyName) &&
-            isPhoneValid(phone = container.state.value.phone) &&
-            isAddressValid(address = container.state.value.address)
+    private fun isFieldsInvalid(): Boolean {
+        val emailError = !isEmailValid(email = container.state.value.email)
+        val passwordError = !isPasswordValid(password = container.state.value.password)
+        val mismatchError = !isConfirmPasswordValid(
+            password = container.state.value.password,
+            confirmPassword = container.state.value.confirmPassword,
+        )
+        val companyNameError = !isCompanyNameValid(companyName = container.state.value.companyName)
+        val phoneError = !isPhoneValid(phone = container.state.value.phone)
+        val addressError = !isAddressValid(address = container.state.value.address)
+
+        return emailError || passwordError || mismatchError || companyNameError ||
+            phoneError || addressError
     }
 
-    private fun handleInputErrorState() {
-        val codeErrors = mutableListOf<CodeError>()
-        var stateError: CreateAccountState?
+    private fun handleInputErrorState() = container.intent {
+        val emailError = !isEmailValid(email = container.state.value.email)
+        val passwordError = !isPasswordValid(password = container.state.value.password)
+        val mismatchError = !isConfirmPasswordValid(
+            password = container.state.value.password,
+            confirmPassword = container.state.value.confirmPassword,
+        )
+        val companyNameError = !isCompanyNameValid(companyName = container.state.value.companyName)
+        val phoneError = !isPhoneValid(phone = container.state.value.phone)
+        val addressError = !isAddressValid(address = container.state.value.address)
 
-        if (!isEmailValid(email = container.state.value.email)) {
-            codeErrors.add(CodeError.INVALID_EMAIL)
-            stateError = container.state.value.copy(emailError = true)
-        }
-
-        if (!isPasswordValid(password = container.state.value.password)) {
-            codeErrors.add(CodeError.INVALID_PASSWORD)
-            stateError = container.state.value.copy(passwordError = true)
-        }
-
-        if (!isConfirmPasswordValid(
-                password = container.state.value.password,
-                confirmPassword = container.state.value.confirmPassword,
-            )
-        ) {
-            codeErrors.add(CodeError.PASSWORD_MISMATCH)
-            stateError = container.state.value.copy(mismatchError = true)
-        } else {
-            stateError = container.state.value.copy(mismatchError = false)
-        }
-
-        if (!isCompanyNameValid(companyName = container.state.value.companyName)) {
-            codeErrors.add(CodeError.INVALID_COMPANY_NAME)
-            stateError = container.state.value.copy(companyNameError = true)
-        }
-
-        if (!isPhoneValid(phone = container.state.value.phone)) {
-            codeErrors.add(CodeError.INVALID_PHONE)
-            stateError = container.state.value.copy(phoneError = true)
-        }
-
-        if (!isAddressValid(address = container.state.value.address)) {
-            codeErrors.add(CodeError.INVALID_ADDRESS)
-            stateError = container.state.value.copy(addressError = true)
-        }
+        val codeErrors = getCodeErrors(
+            emailError = emailError,
+            passwordError = passwordError,
+            mismatchError = mismatchError,
+            companyNameError = companyNameError,
+            phoneError = phoneError,
+            addressError = addressError,
+        )
 
         // If has more than one error set a generic error to fix the invalid fields
-        if (codeErrors.size > 1) {
-            setErrorMessage(
-                codeError = CodeError.INVALID_FIELDS,
-                newState = container.state.value.copy(fieldsError = true),
+        var fieldsError = false
+        if (codeErrors.size > 1) fieldsError = true
+
+        reduce {
+            copy(
+                emailError = emailError,
+                passwordError = passwordError,
+                mismatchError = mismatchError,
+                companyNameError = companyNameError,
+                phoneError = phoneError,
+                addressError = addressError,
+                fieldsError = fieldsError,
             )
-        } else {
-            codeErrors.lastOrNull()?.let {
-                setErrorMessage(
-                    codeError = it,
-                    newState = stateError,
-                )
-            }
+        }
+
+        codeErrors.lastOrNull()?.let {
+            postSideEffect(effect = CreateAccountSideEffect.ShowError(codeError = it))
         }
     }
 
-    private fun setErrorMessage(codeError: CodeError, newState: CreateAccountState) =
-        container.intent {
-            reduce { newState }
-            postSideEffect(effect = CreateAccountSideEffect.ShowError(codeError = codeError))
-        }
+    private fun getCodeErrors(
+        emailError: Boolean,
+        passwordError: Boolean,
+        mismatchError: Boolean,
+        companyNameError: Boolean,
+        phoneError: Boolean,
+        addressError: Boolean,
+    ): List<CodeError> {
+        val codeErrors = mutableListOf<CodeError>()
+
+        if (emailError) codeErrors.add(CodeError.INVALID_EMAIL)
+
+        if (passwordError) codeErrors.add(CodeError.INVALID_PASSWORD)
+
+        if (mismatchError) codeErrors.add(CodeError.PASSWORD_MISMATCH)
+
+        if (companyNameError) codeErrors.add(CodeError.INVALID_COMPANY_NAME)
+
+        if (phoneError) codeErrors.add(CodeError.INVALID_PHONE)
+
+        if (addressError) codeErrors.add(CodeError.INVALID_ADDRESS)
+
+        if (codeErrors.size > 1) codeErrors.add(CodeError.INVALID_FIELDS)
+
+        return codeErrors
+    }
 }
