7bc01f5e71b05f6cd03aa6182a3dfaf7faa458ca
bruno13palhano <bruno13palhano@gmail.com>
2025-02-10 13:13:41 -0300

    Add methods to handle date

---
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/shared/DateUtils.kt
@@ -0,0 +1,14 @@
+package com.bruno13palhano.mais1venda.ui.screens.shared
+
+import android.icu.text.DateFormat
+import android.icu.text.SimpleDateFormat
+import android.icu.util.TimeZone
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+
+fun currentDate() = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli()
+
+val dateFormat: DateFormat =
+    SimpleDateFormat.getDateInstance().apply {
+        timeZone = TimeZone.getTimeZone("UTC")
+    }
