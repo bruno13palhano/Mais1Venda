fd351f73db0bf61d7704a98d964d056acabef74d
bruno13palhano <bruno13palhano@gmail.com>
2025-02-17 21:07:03 -0300

    Inject CustomerRepository

--- data/src/main/java/com/bruno13palhano/data/di/RepositoryModule.kt
+++ data/src/main/java/com/bruno13palhano/data/di/RepositoryModule.kt
@@ -2,6 +2,8 @@ package com.bruno13palhano.data.di
 
 import com.bruno13palhano.data.repository.CompanyRepository
 import com.bruno13palhano.data.repository.CompanyRepositoryImpl
+import com.bruno13palhano.data.repository.CustomerRepository
+import com.bruno13palhano.data.repository.CustomerRepositoryImpl
 import com.bruno13palhano.data.repository.OrderRepository
 import com.bruno13palhano.data.repository.OrderRepositoryImpl
 import com.bruno13palhano.data.repository.ProductRepository
@@ -12,31 +14,28 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-annotation class CompanyRep
-
-annotation class OrderRep
-
-annotation class ProductRep
-
 @Module
 @InstallIn(SingletonComponent::class)
 internal abstract class RepositoryModule {
     @Binds
-    @CompanyRep
     @Singleton
     abstract fun bindCompanyRepository(
         companyRepositoryImpl: CompanyRepositoryImpl,
     ): CompanyRepository
 
     @Binds
-    @OrderRep
     @Singleton
     abstract fun bindOrderRepository(orderRepositoryImpl: OrderRepositoryImpl): OrderRepository
 
     @Binds
-    @ProductRep
     @Singleton
     abstract fun bindProductRepository(
         productRepositoryImpl: ProductRepositoryImpl,
     ): ProductRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCustomerRepository(
+        customerRepositoryImpl: CustomerRepositoryImpl,
+    ): CustomerRepository
 }
