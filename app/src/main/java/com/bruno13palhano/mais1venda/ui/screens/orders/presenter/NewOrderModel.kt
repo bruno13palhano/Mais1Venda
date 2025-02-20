07e0b72721e4c292354d8400c6e35e0b27142683
bruno13palhano <bruno13palhano@gmail.com>
2025-02-19 21:06:10 -0300

    Add events to confirm and cancel new order and side effects to show erros

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/presenter/NewOrderModel.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/presenter/NewOrderModel.kt
@@ -1,19 +1,26 @@
 package com.bruno13palhano.mais1venda.ui.screens.orders.presenter
 
 import androidx.compose.runtime.Immutable
+import com.bruno13palhano.data.model.shared.Order
+import com.bruno13palhano.mais1venda.ui.screens.authentication.shared.CodeError
 
 @Immutable
 internal data class NewOrderState(
-    val id: Long = 0L,
+    val order: Order? = null,
 )
 
 @Immutable
 internal sealed interface NewOrderEvent {
     data class LoadOrder(val id: Long) : NewOrderEvent
+    data object ConfirmOrder : NewOrderEvent
+    data object CancelOrder : NewOrderEvent
+    data object DismissKeyboard : NewOrderEvent
     data object NavigateBack : NewOrderEvent
 }
 
 @Immutable
 internal sealed interface NewOrderSideEffect {
+    data class ShowError(val codeError: CodeError) : NewOrderSideEffect
+    data object DismissKeyboard : NewOrderSideEffect
     data object NavigateBack : NewOrderSideEffect
 }
