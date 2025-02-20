8f3c60f0f905dab59301da90a12a4d8b6a17e82e
bruno13palhano <bruno13palhano@gmail.com>
2025-02-20 16:57:19 -0300

    Implement OrderRemoteData

--- data/src/main/java/com/bruno13palhano/data/datasource/remote/source/OrderRemoteData.kt
+++ data/src/main/java/com/bruno13palhano/data/datasource/remote/source/OrderRemoteData.kt
@@ -1,25 +1,24 @@
 package com.bruno13palhano.data.datasource.remote.source
 
+import com.bruno13palhano.data.datasource.remote.service.ApiService
+import com.bruno13palhano.data.model.resource.Resource
 import com.bruno13palhano.data.model.shared.Order
+import javax.inject.Inject
 
-class OrderRemoteData {
-    suspend fun insert(order: Order): Order {
-        TODO("Not yet implemented")
+internal class OrderRemoteData @Inject constructor(private val api: ApiService) {
+    suspend fun getAll(): Resource<List<Order>> {
+        return safeApiCall { api.getAllOrders() }
     }
 
-    suspend fun update(order: Order): Order {
-        TODO("Not yet implemented")
+    suspend fun getNewOrders(): Resource<List<Order>> {
+        return safeApiCall { api.getAllNewOrders() }
     }
 
-    suspend fun deleteById(id: Long) {
-        TODO("Not yet implemented")
+    suspend fun confirmOrder(id: Long): Resource<Boolean> {
+        return safeApiCall { api.confirmOrder(id = id) }
     }
 
-    suspend fun getById(id: Long): Order? {
-        TODO("Not yet implemented")
-    }
-
-    suspend fun getAll(): List<Order> {
-        TODO("Not yet implemented")
+    suspend fun cancelOrder(id: Long): Resource<Boolean> {
+        return safeApiCall { api.cancelOrder(id = id) }
     }
 }
