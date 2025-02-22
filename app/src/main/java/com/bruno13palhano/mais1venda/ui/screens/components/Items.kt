1c1e67656c2b493e4534b88be76504e1476793ce
bruno13palhano <bruno13palhano@gmail.com>
2025-02-22 13:25:06 -0300

    Extract common order info and data related to previews to a separeted file

---
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/components/Items.kt
@@ -0,0 +1,94 @@
+package com.bruno13palhano.mais1venda.ui.screens.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.bruno13palhano.data.model.company.Product
+import com.bruno13palhano.data.model.customer.Customer
+import com.bruno13palhano.data.model.shared.Address
+import com.bruno13palhano.data.model.shared.Order
+import com.bruno13palhano.data.model.shared.OrderStatus
+import com.bruno13palhano.mais1venda.R
+import com.bruno13palhano.mais1venda.ui.screens.shared.dateFormat
+
+@Composable
+internal fun OrderListItem(
+    customerName: String,
+    productName: String,
+    orderDate: Long,
+    price: Float,
+    onClick: () -> Unit,
+) {
+    ListItem(
+        modifier = Modifier
+            .clickable { onClick() }
+            .padding(4.dp),
+        overlineContent = {
+            Text(
+                text = customerName,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        },
+        headlineContent = {
+            Text(
+                text = productName,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        },
+        supportingContent = {
+            Text(
+                text = dateFormat.format(orderDate),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        },
+        trailingContent = {
+            Text(
+                text = stringResource(id = R.string.price_tag, price),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+            )
+        }
+    )
+}
+
+val testProduct = Product(
+    id = 1,
+    name = "Product 1",
+    price = 1.0f,
+    category = emptyList(),
+    description = "Description 1",
+    code = "1",
+    quantity = 1,
+    exhibitToCatalog = false,
+    lastModifiedTimestamp = "",
+)
+
+val testCustomer = Customer(
+    uid = "1",
+    name = "Customer 1",
+    email = "email 1",
+    phone = "phone 1",
+    address = Address("", "", "", ""),
+    socialMedia = emptyList(),
+    orders = emptyList(),
+    lastModifiedTimestamp = "",
+)
+
+val testOrder = Order(
+    id = 1,
+    product = testProduct,
+    customer = testCustomer,
+    orderDate = 111111111111212,
+    deliveryDate = 12234523453245,
+    status = OrderStatus.PROCESSING_ORDER,
+    lastModifiedTimestamp = "",
+)
--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/presenter/NewOrdersScreen.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/presenter/NewOrdersScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -28,20 +27,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.bruno13palhano.data.model.company.Product
-import com.bruno13palhano.data.model.customer.Customer
-import com.bruno13palhano.data.model.shared.Address
-import com.bruno13palhano.data.model.shared.Order
-import com.bruno13palhano.data.model.shared.OrderStatus
 import com.bruno13palhano.mais1venda.R
 import com.bruno13palhano.mais1venda.ui.screens.components.CircularProgress
+import com.bruno13palhano.mais1venda.ui.screens.components.OrderListItem
+import com.bruno13palhano.mais1venda.ui.screens.components.testCustomer
+import com.bruno13palhano.mais1venda.ui.screens.components.testOrder
 import com.bruno13palhano.mais1venda.ui.screens.orders.viewmodel.NewOrdersViewModel
-import com.bruno13palhano.mais1venda.ui.screens.shared.dateFormat
 import com.bruno13palhano.mais1venda.ui.screens.shared.rememberFlowWithLifecycle
 import com.bruno13palhano.mais1venda.ui.theme.Mais1VendaTheme
 import kotlinx.coroutines.launch
@@ -133,36 +128,12 @@ private fun NewOrdersContent(
                 contentPadding = PaddingValues(4.dp),
             ) {
                 items(items = state.newOrders, key = { order -> order.id }) { order ->
-                    ListItem(
-                        modifier = Modifier.padding(4.dp),
-                        overlineContent = {
-                            Text(
-                                text = order.customer.name,
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                            )
-                        },
-                        headlineContent = {
-                            Text(
-                                text = order.product.name,
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                            )
-                        },
-                        supportingContent = {
-                            Text(
-                                text = dateFormat.format(order.orderDate),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                            )
-                        },
-                        trailingContent = {
-                            Text(
-                                text = stringResource(id = R.string.price_tag, order.product.price),
-                                maxLines = 1,
-                                overflow = TextOverflow.Ellipsis,
-                            )
-                        }
+                    OrderListItem(
+                        customerName = order.customer.name,
+                        productName = order.product.name,
+                        orderDate = order.orderDate,
+                        price = order.product.price,
+                        onClick = { onEvent(NewOrdersEvent.NavigateToNewOrder(id = order.id)) }
                     )
                 }
             }
@@ -182,14 +153,16 @@ private fun NewOrdersPreview() {
                 snackbarHostState = remember { SnackbarHostState() },
                 state = NewOrdersState(
                     newOrders = listOf(
-                        order,
-                        order.copy(id = 2, customer = customer.copy(name = "Customer 2")),
-                        order.copy(id = 3, customer = customer.copy(name = "Customer 3")),
-                        order.copy(id = 4, customer = customer.copy(name = "Customer 4")),
-                        order.copy(id = 5, customer = customer.copy(name = "Customer 5")),
-                        order.copy(id = 6, customer = customer.copy(name = "Customer 6")),
-                        order.copy(id = 7, customer = customer.copy(name = "Customer 7")),
-                        order.copy(id = 8, customer = customer.copy(name = "Customer 8")),
+                        testOrder,
+                        testOrder.copy(id = 2, customer = testCustomer.copy(name = "Customer 2")),
+                        testOrder.copy(id = 3, customer = testCustomer.copy(name = "Customer 3")),
+                        testOrder.copy(id = 4, customer = testCustomer.copy(name = "Customer 4")),
+                        testOrder.copy(id = 5, customer = testCustomer.copy(name = "Customer 5")),
+                        testOrder.copy(id = 6, customer = testCustomer.copy(name = "Customer 6")),
+                        testOrder.copy(id = 7, customer = testCustomer.copy(name = "Customer 7")),
+                        testOrder.copy(id = 8, customer = testCustomer.copy(name = "Customer 8")),
+                        testOrder.copy(id = 9, customer = testCustomer.copy(name = "Customer 9")),
+                        testOrder.copy(id = 10, customer = testCustomer.copy(name = "Customer 10")),
                     ),
                 ),
                 onEvent = {},
@@ -214,36 +187,3 @@ private fun NewOrdersLoadingPreview() {
         }
     }
 }
-
-private val product = Product(
-    id = 1,
-    name = "Product 1",
-    price = 1120000.50f,
-    category = emptyList(),
-    description = "Description 1",
-    code = "1",
-    quantity = 1,
-    exhibitToCatalog = false,
-    lastModifiedTimestamp = "",
-)
-
-private val customer = Customer(
-    uid = "1",
-    name = "Customer 1",
-    email = "email 1",
-    phone = "phone 1",
-    address = Address("", "", "", ""),
-    socialMedia = emptyList(),
-    orders = emptyList(),
-    lastModifiedTimestamp = "",
-)
-
-private val order = Order(
-    id = 1,
-    product = product,
-    customer = customer,
-    orderDate = 111111111111212,
-    deliveryDate = 12234523453245,
-    status = OrderStatus.PROCESSING_ORDER,
-    lastModifiedTimestamp = "",
-)
