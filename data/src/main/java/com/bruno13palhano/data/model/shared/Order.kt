91bdaf106a76e0042c0240a10a4b9de3e9faa9b7
bruno13palhano <bruno13palhano@gmail.com>
2025-02-26 10:49:44 -0300

    Replace product properties and add new ones

--- data/src/main/java/com/bruno13palhano/data/model/shared/Order.kt
+++ data/src/main/java/com/bruno13palhano/data/model/shared/Order.kt
@@ -1,11 +1,15 @@
 package com.bruno13palhano.data.model.shared
 
-import com.bruno13palhano.data.model.company.Product
 import com.bruno13palhano.data.model.customer.Customer
 
 data class Order(
     val id: Long,
-    val product: Product,
+    val productName: String,
+    val productCode: String,
+    val quantity: Int,
+    val unitPrice: Double,
+    val off: Float,
+    val totalPrice: Double,
     val customer: Customer,
     val orderDate: Long,
     val deliveryDate: Long,
