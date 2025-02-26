5eb53c670fb8c7f462fa06ce107e8e53ba81a411
bruno13palhano <bruno13palhano@gmail.com>
2025-02-26 10:51:02 -0300

    Add a entity for Ad

---
+++ data/src/main/java/com/bruno13palhano/data/datasource/local/entity/AdEntity.kt
@@ -0,0 +1,21 @@
+package com.bruno13palhano.data.datasource.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ad")
+internal data class AdEntity(
+    @PrimaryKey
+    val id: Long,
+    val title: String,
+    val productId: Long,
+    val price: Double,
+    val description: String,
+    val observations: String,
+    val off: Float,
+    val stockQuantity: Int,
+    val unitsSold: Int,
+    val questions: List<String>,
+    val reviews: List<String>,
+    val lastModifiedTimestamp: String,
+)
