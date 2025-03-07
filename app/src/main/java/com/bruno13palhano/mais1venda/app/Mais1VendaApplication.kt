e7478615f63b4d232c73adb5a7b49800dbbbbc82
bruno13palhano <bruno13palhano@gmail.com>
2025-03-06 22:36:08 -0300

    Cofig worker

--- app/src/main/java/com/bruno13palhano/mais1venda/app/Mais1VendaApplication.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/app/Mais1VendaApplication.kt
@@ -1,7 +1,18 @@
 package com.bruno13palhano.mais1venda.app
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class Mais1VendaApplication : Application()
+class Mais1VendaApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory = workerFactory)
+            .build()
+}
