bfefc601c50d5bd66e36635cec8e2b350a4a4e7d
bruno13palhano <bruno13palhano@gmail.com>
2025-03-07 22:17:49 -0300

    Add Sync related classes

--- data/src/main/java/com/bruno13palhano/data/sync/Sync.kt
+++ data/src/main/java/com/bruno13palhano/data/sync/Sync.kt
@@ -1,4 +1,19 @@
 package com.bruno13palhano.data.sync
 
+import android.content.Context
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
+
 object Sync {
+    fun initializer(context: Context) {
+        WorkManager.getInstance(context).apply {
+            enqueueUniqueWork(
+                SYNC_WORK_NAME,
+                ExistingWorkPolicy.KEEP,
+                SyncDataWork.startUpSyncWork()
+            )
+        }
+    }
 }
+
+internal const val SYNC_WORK_NAME = "SyncDatabaseWorker"
---
+++ data/src/main/java/com/bruno13palhano/data/sync/SyncDataWork.kt
@@ -0,0 +1,47 @@
+package com.bruno13palhano.data.sync
+
+import android.content.Context
+import androidx.hilt.work.HiltWorker
+import androidx.work.Constraints
+import androidx.work.CoroutineWorker
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkerParameters
+import com.bruno13palhano.data.repository.OrderRepository
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+
+@HiltWorker
+internal class SyncDataWork @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters,
+    private val orderRepository: OrderRepository,
+) : CoroutineWorker(context, params), Synchronizer {
+    override suspend fun doWork(): Result {
+        val syncStatus = orderRepository.sync()
+
+        if (syncStatus) {
+            return Result.success()
+        } else {
+            return Result.retry()
+        }
+    }
+
+    companion object {
+        fun startUpSyncWork() = OneTimeWorkRequestBuilder<SyncDataWork>()
+            .setConstraints(
+                Constraints.Builder()
+                    .setRequiredNetworkType(NetworkType.CONNECTED)
+                    .build()
+            )
+            .build()
+    }
+}
+
+interface Synchronizer {
+    suspend fun Syncable.sync() = this@sync.syncWith(this@Synchronizer)
+}
+
+interface Syncable {
+    suspend fun syncWith(synchronizer: Synchronizer): Boolean
+}
