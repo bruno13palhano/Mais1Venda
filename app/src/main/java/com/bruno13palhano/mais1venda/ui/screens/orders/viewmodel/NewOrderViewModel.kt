612cd540f1062c4f91a8866f9d3ee14815839d7b
bruno13palhano <bruno13palhano@gmail.com>
2025-03-17 21:53:50 -0300

    Load order from notification and confirmOrder method has been implemented

--- app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/viewmodel/NewOrderViewModel.kt
+++ app/src/main/java/com/bruno13palhano/mais1venda/ui/screens/orders/viewmodel/NewOrderViewModel.kt
@@ -2,8 +2,12 @@ package com.bruno13palhano.mais1venda.ui.screens.orders.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bruno13palhano.data.model.resource.Resource
 import com.bruno13palhano.data.mvi.Container
+import com.bruno13palhano.data.notifications.EventBus
+import com.bruno13palhano.data.notifications.OrderEvent
 import com.bruno13palhano.data.repository.OrderRepository
+import com.bruno13palhano.mais1venda.ui.screens.authentication.shared.CodeError
 import com.bruno13palhano.mais1venda.ui.screens.orders.presenter.NewOrderEvent
 import com.bruno13palhano.mais1venda.ui.screens.orders.presenter.NewOrderSideEffect
 import com.bruno13palhano.mais1venda.ui.screens.orders.presenter.NewOrderState
@@ -14,6 +18,7 @@ import javax.inject.Inject
 internal class NewOrderViewModel @Inject constructor(
     initialState: NewOrderState,
     private val orderRepository: OrderRepository,
+    private val eventBus: EventBus,
 ) : ViewModel() {
     val container = Container<NewOrderState, NewOrderSideEffect>(
         initialState = initialState,
@@ -37,11 +42,52 @@ internal class NewOrderViewModel @Inject constructor(
     }
 
     private fun loadOrder(id: Long) = container.intent {
-        val order = orderRepository.get(id = id)
-        reduce { copy(order = order) }
+        eventBus.events.collect { eventBus ->
+            when (eventBus) {
+                is OrderEvent.OrderCreated -> {
+                    if (eventBus.order.id == id) {
+                        reduce { copy(order = eventBus.order) }
+                    }
+                }
+                else -> {}
+            }
+        }
     }
 
     private fun confirmOrder() = container.intent {
+        container.state.value.order?.id?.let {
+            reduce { copy(isLoading = true) }
+            val result = orderRepository.confirmOrder(id = it)
+
+            when (result) {
+                is Resource.Success -> {
+                    postSideEffect(effect = NewOrderSideEffect.NavigateBack)
+                    reduce { copy(isLoading = false) }
+                }
+
+                is Resource.ResponseError -> {
+                    reduce { copy(isLoading = false) }
+                    result.remoteResponseError?.let { remoteResponseError ->
+                        postSideEffect(
+                            effect = NewOrderSideEffect.ShowError(
+                                codeError = CodeError.UNKNOWN_ERROR,
+                            ),
+                        )
+                    }
+                }
+
+                is Resource.Error -> {
+                    reduce { copy(isLoading = false) }
+                    result.errorType?.let { errorType ->
+                        postSideEffect(
+                            effect = NewOrderSideEffect.ShowError(
+                                codeError = CodeError.UNKNOWN_ERROR,
+                            ),
+                        )
+                    }
+                }
+            }
+        }
     }
 
     private fun cancelOrder() = container.intent {
